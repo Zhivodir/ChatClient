43a435e39f0d3ec8fa7b7de2e1cc780d07552df4
zhivodir <dzhivchik@gmail.com>
2016-12-18 19:16:36 +0200

    ChatClient: authorization and private message

--- src/ua/kiev/prog/GetThread.java
+++ src/ua/kiev/prog/GetThread.java
@@ -13,16 +13,18 @@ import java.nio.charset.StandardCharsets;
 public class GetThread implements Runnable {
     private final Gson gson;
     private int n;
+    private String loginOfOwner;
 
-    public GetThread() {
+    public GetThread(String login){
         gson = new GsonBuilder().create();
+        this.loginOfOwner = login;
     }
 
     @Override
     public void run() {
         try {
             while ( ! Thread.interrupted()) {
-                URL url = new URL(Utils.getURL() + "/get?from=" + n);
+                URL url = new URL(Utils.getURL() + "/get?from=" + n + "&login=" + loginOfOwner);
                 HttpURLConnection http = (HttpURLConnection) url.openConnection();
 
                 InputStream is = http.getInputStream();
--- src/ua/kiev/prog/Main.java
+++ src/ua/kiev/prog/Main.java
@@ -19,7 +19,7 @@ public class Main {
 				pswd = scanner.nextLine();
 			}while(sendReq("/authorization?login=" + login + "&password=" + pswd) != 200);
 
-			Thread thread = new Thread(new GetThread());
+			Thread thread = new Thread(new GetThread(login));
 			thread.setDaemon(true);
 			thread.start();
 
@@ -27,8 +27,14 @@ public class Main {
 			while (true) {
 				String text = scanner.nextLine();
 				if (text.isEmpty()) break;
-
-				Message m = new Message(login, text);
+				Message m;
+                if(text.contains("::")){
+                    int cut = text.indexOf("::");
+                    m = new Message(login, text.substring(cut + 2));
+                    m.setTo(text.substring(0, cut).trim());
+                }else{
+                    m = new Message(login, text);
+                }
 				int res = m.send(Utils.getURL() + "/add");
 
 				if (res != 200) { // 200 OK
